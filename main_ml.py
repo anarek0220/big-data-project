7e3811425804e42c4e37d869ed30a53de52dae39
anarek0220 <116803796+anarek0220@users.noreply.github.com>
2023-01-20 00:14:29 +0400

    Add files via upload

--- main_ml.py
+++ main_ml.py
@@ -7,19 +7,23 @@ import matplotlib.pyplot as plt
 import glob as gb
 import os
 
+testpath_2 = "test_1/"
+load_model = tf.keras.models.load_model(
+    "model/", custom_objects=None, compile=True, options=None
+)
+
+
 trainpath = "train/"
 testpath = "test/"
 predpath = "predict/"
-print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
+
 len(trainpath)
 
 for folder in os.listdir(trainpath):
     files = gb.glob(pathname=str(trainpath+folder + "/*.jpeg"))
-    print(f"for training data , found {len(files)} in folder {folder}")
 
 for folder in os.listdir(testpath):
     files = gb.glob(pathname=str(testpath+folder + "/*.jpeg"))
-    print(f"for training data , found {len(files)} in folder {folder}")
 
 
 files = gb.glob(pathname=str(predpath+"/*.jpeg"))
@@ -72,7 +76,7 @@ pd.DataFrame(df.value_counts())[:30]
 s = 150
 x_test = []
 y_test = []
-#os.environ["KERAS_BACKEND"] = "plaidml.keras.backend"
+
 for folder in os.listdir(testpath):
     files = gb.glob(pathname=str(testpath+folder + "/*.jpeg"))
     for file in files:
@@ -115,7 +119,7 @@ for file in files:
 len(x_pred)
 
 plt.figure(figsize=(20 , 20))
-for o , p in enumerate(list(np.random.randint(0 , 48 ,36))):
+for o , p in enumerate(list(np.random.randint(0 , 1 ,36))):
     plt.subplot(6 , 6 , o+1)
     plt.imshow(x_pred[p])
     plt.axis("off")
@@ -125,48 +129,20 @@ y_train = np.array(y_train)
 x_pred_array = np.array(x_pred)
 x_test = np.array(x_test)
 y_test = np.array(y_test)
-print(x_test.shape)
-model = keras.models.Sequential([
-    keras.layers.Conv2D(250, kernel_size=(3, 3), activation="relu", input_shape=(s, s, 3)),
-    keras.layers.Conv2D(230, kernel_size=(3, 3), activation="relu"),
-    keras.layers.Conv2D(200, kernel_size=(3, 3), activation="relu"),
-    keras.layers.MaxPool2D(6, 6),
-    keras.layers.Conv2D(180, kernel_size=(3, 3), activation="relu"),
-    keras.layers.Conv2D(100, kernel_size=(3, 3), activation="relu"),
-    keras.layers.Conv2D(80, kernel_size=(3, 3), activation="relu"),
-    keras.layers.MaxPool2D(6, 6),
-    keras.layers.Flatten(),
-    keras.layers.Dense(120, activation="relu"),
-    keras.layers.Dense(100, activation="relu"),
-    keras.layers.Dense(50, activation="relu"),
-    keras.layers.Dropout(rate=0.3),
-    keras.layers.Dense(10, activation="softmax"),
 
-])
+y_result = load_model.predict(x_pred_array)
 
-model.compile(optimizer="adam" , loss="sparse_categorical_crossentropy" , metrics=["accuracy"])
-model.summary()
 
-model.fit(x_train , y_train , epochs=30 , batch_size = 64)
-
-modelloss , modelaccuracy = model.evaluate(x_test , y_test)
-y_pred = model.predict(x_test)
-y_result = model.predict(x_pred_array)
-y_result.shape
 plt.figure(figsize=(20 , 20))
-for n , i in enumerate(list(np.random.randint(0 , 48 , 36))):
+for n , i in enumerate(list(np.random.randint(0 , 1 , 36))):
     plt.subplot(6 , 6 , n+1)
     plt.imshow(x_pred[i])
     plt.axis("off")
     plt.title(getcode_train(np.argmax(y_result[i])))
 
-tf.keras.models.save_model(
-        model,
-        "model/",
-        overwrite=True,
-        include_optimizer=True,
-        save_format=None,
-        signatures=None,
-        options=None,
-        save_traces=True
-)
+result_dic = {0: "apple", 1: "avocado", 2: "banana", 3: "cherry", 4: "kiwi", 5: "mango", 6: "orange", 7: "pinenapple", 8: "stawberries", 9: "watermelon"}
+
+for abc in y_result:
+    print(result_dic[abc.argmax()])
+
+
